b024d76d6d8f7458abb7ecd0c71327a3d544f6ae
Neeraj Yadav <yneeraj9111@gmail.com>
2022-02-25 13:18:16 +0530

    cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
+var cors = require('cors')
 const express = require('express');
 const connect = require('./confiq/db')
 const app = express();
 const path = require('path')
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(express.json());
-
+app.use(cors())
 const userController = require('./controllers/user.controller')
 const productController = require('./controllers/product.controller')
 const cataController = require('./controllers/category.controller')
